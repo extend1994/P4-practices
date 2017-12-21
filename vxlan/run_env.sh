ddffca686ae66be9e7f53531e48421880efa6f40
LboAnn <extend1994@gmail.com>
2017-12-21 20:26:13 +0800

    Descript running program & reset counter when starting mininet with color

--- vxlan/run_env.sh
+++ vxlan/run_env.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
+CLI_PATH=../../bmv2/tools/runtime_CLI.py
+SWITCH_PATH=../../bmv2/targets/simple_router/simple_router
+
 if [ $# -lt 1 ]; then
   echo "Usage: . run.sh filename"
 else
   # Remove generated json files before regernating it
   rm -f *.json
+  echo.LightBlue "Compile $1.p4"
   p4c-bmv2 --json $1.json $1.p4
-  sudo python ../../bmv2/mininet/1sw_demo.py --behavioral-exe ../../bmv2/targets/simple_router/simple_router --json $1.json &
+  echo.LightBlue "Resetting mininet environment..."
+  sudo mn -c
+  echo.LightBlue "Turn on mininet environment..."
+  sudo python ../../bmv2/mininet/1sw_demo.py --behavioral-exe ../../bmv2/targets/simple_router/simple_router --json vxlan.json &
   sleep 1
-  ../../bmv2/tools/runtime_CLI.py --json $1.json --thrift-port 9090 < commands.txt
-  echo "Ready!"
+  echo.LightBlue "Apply entry rules to switch"
+  $CLI_PATH --json vxlan.json < commands.txt
+  echo.LightBlue "Resetting all counters..."
+  echo "counter_reset inner_udp_counter" | $CLI_PATH --json vxlan.json
+  echo "counter_reset inner_tcp_counter" | $CLI_PATH --json vxlan.json
+  echo "counter_reset inner_icmp_counter" | $CLI_PATH --json vxlan.json
+  echo "Reset done!"
   fg
 fi
