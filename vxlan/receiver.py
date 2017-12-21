321c5683920a63cdf85c80d8895d9f60d84c8190
LboAnn <extend1994@gmail.com>
2017-12-21 20:29:43 +0800

    Send more pkts to test switch behavior & add receiver error handler

--- vxlan/receiver.py
+++ vxlan/receiver.py
@@ -4,13 +4,28 @@ from scapy.all import sniff
 import sys
 import struct
 
+class colors:
+  DEFAULT   = '\033[0m' # white
+  BOLDER    = '\033[1m'
+  UNDERLINE = '\033[4m'
+  RED     = '\033[91m'
+  GREEN   = '\033[92m'
+  YELLOW  = '\033[93m'
+  BLUE    = '\033[94m'
+  PURPLE  = '\033[95m'
+
 def handle_pkt(packet):
+  print colors.YELLOW + "New packet!"
+  print colors.DEFAULT
   load_contrib('vxlan') # enable vxlan use in scapy package
-  packet[UDP].decode_payload_as(VXLAN) # change the way of raw data decoding
-  packet.show() # show the received packet info
+  try:
+    packet[UDP].decode_payload_as(VXLAN) # change the way of raw data decoding
+  except:
+    print colors.RED
+    print "Unexpected"
 
 def main():
-  sniff(iface = "eth0", prn = lambda x: handle_pkt(x))
+  sniff(filter="dst 10.0.1.10", iface = "eth0", prn = lambda x: handle_pkt(x))
 
 if __name__ == '__main__':
   main()
--- vxlan/send.py
+++ vxlan/send.py
@@ -1,15 +1,49 @@
 #!/usr/bin/python
 from scapy.all import *
 
+class colors:
+  DEFAULT   = '\033[0m' # white
+  BOLDER    = '\033[1m'
+  UNDERLINE = '\033[4m'
+  RED     = '\033[91m'
+  GREEN   = '\033[92m'
+  YELLOW  = '\033[93m'
+  BLUE    = '\033[94m'
+  PURPLE  = '\033[95m'
+
 def main():
   # Assign mac and ip address according the topology bmv2/mininet/1sw_demo.py built
-  h1_mac = "00:04:00:00:00:00"
-  h2_mac = "00:04:00:00:00:01"
-  h1_ip  = "10.0.0.10"
-  h2_ip  = "10.0.1.10"
+  h1_mac    = "00:04:00:00:00:00"
+  h2_mac    = "00:04:00:00:00:01"
+  in_h1_mac = "00:04:00:00:00:02"
+  in_h2_mac = "00:04:00:00:00:03"
+  h1_ip     = "10.0.0.10"
+  h2_ip     = "10.0.1.10"
+  in_h1_ip  = "10.0.2.10"
+  in_h2_ip  = "10.0.3.10"
   load_contrib('vxlan') # enable vxlan use in scapy package
-  pkt = Ether(dst=h2_mac, src=h1_mac)/IP(dst=h2_ip, src=h1_ip)/UDP()/VXLAN(vni=61)/Raw(load="\x00\x01\x00\x02\x01")
-  pkt.show() # show the packet info before senting the packet
+  pkt = Ether(dst=h2_mac,src=h1_mac)/IP(dst=h2_ip,src=h1_ip)/UDP()/VXLAN(vni=61)/\
+        Ether(dst=in_h2_mac, src=in_h1_mac)/IP(dst=in_h2_ip,src=in_h1_ip)/UDP()/\
+        Raw(load="\x00\x01\x00\x02\x01")
+  print colors.YELLOW
+  print "Send a VXLAN(VNI=61) packet with " + str(len(pkt[VXLAN][Ether])) + " bytes inner UDP frame"
+  print colors.DEFAULT
+  sendp(pkt, iface = "eth0")
+
+  pkt = Ether(dst=h2_mac,src=h1_mac)/IP(dst=h2_ip,src=h1_ip)/UDP()/VXLAN(vni=60)/\
+        Ether(dst=in_h2_mac, src=in_h1_mac)/IP(dst=in_h2_ip,src=in_h1_ip)/TCP()/\
+        Raw(load="\x00\x01\x00\x02\x01")
+  print colors.YELLOW
+  print "Send a VXLAN(VNI=60) packet with " + str(len(pkt[VXLAN][Ether])) + " bytes inner TCP frame"
+  print colors.DEFAULT
+  sendp(pkt, iface = "eth0")
+
+  pkt = Ether(dst=h2_mac,src=h1_mac)/IP(dst=h2_ip,src=h1_ip)/UDP()/VXLAN(vni=61)/\
+        Ether(dst=in_h2_mac, src=in_h1_mac)/IP(dst=in_h2_ip,src=in_h1_ip)/ICMP()/\
+        Raw(load="\x00\x01\x00\x02\x01")
+  print colors.YELLOW
+  print "Send a VXLAN(VNI=61) packet with " + str(len(pkt[VXLAN][Ether])) + " bytes inner ICMP frame"
+  print colors.DEFAULT
   sendp(pkt, iface = "eth0")
 
 if __name__ == '__main__':
